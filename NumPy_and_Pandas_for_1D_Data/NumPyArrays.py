4b562a570dc81e432a5e27039cdba4038254a52a
przemek <jedrfam@gmail.com>
2023-06-18 18:42:53 +0200

    added files ,and NumPyArrays.py collection

---
+++ NumPy_and_Pandas_for_1D_Data/NumPyArrays.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# First 20 countries with employment data
+countries = np.array([
+    'Afghanistan', 'Albania', 'Algeria', 'Angola', 'Argentina',
+    'Armenia', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas',
+    'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium',
+    'Belize', 'Benin', 'Bhutan', 'Bolivia',
+    'Bosnia and Herzegovina'
+])
